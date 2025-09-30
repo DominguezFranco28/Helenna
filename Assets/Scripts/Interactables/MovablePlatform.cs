0c8039eac7b1ae5431ef12b4a68157509172ad1d
Franco Dominguez <francosmg08@gmail.com>
2025-09-30 14:50:20 -0300

    fix platforms verticality

--- Assets/Scripts/Interactables/MovablePlatform.cs
+++ Assets/Scripts/Interactables/MovablePlatform.cs
@@ -1,18 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using static UnityEngine.EventSystems.EventTrigger;
 
 public class MovablePlatform : MonoBehaviour , IMovable
 {
     public bool elevatorTriggered = false;
     public Transform dismountPosition;
     [SerializeField] private Vector2 elevatedPosition;
-
+    [SerializeField] private float usableWidth = 2.0f;  // ancho util de la plataforma para posisionar a los pjs
     [SerializeField] private float _moveSmoothTime;
     [SerializeField] private float speed = 5f;       // Movement speed
     [SerializeField] private float smoothTime = 0.1f; // Smoothing factors
 
     public List<CharacterVerticalCollider> characters = new List<CharacterVerticalCollider>();
+    public List<GameObject> charactersTransform = new List<GameObject>();
     private Vector2 groundPosition;
     private Vector2 _target;
     private Vector2 _velocity = Vector2.zero;
@@ -33,7 +35,7 @@ public class MovablePlatform : MonoBehaviour , IMovable
         {
             elevatorTriggered = false;
             elevatorMoving = true;
-
+            RandomPosInPlatform();
             if (onGround)
             {
                 //Go Up
@@ -61,11 +63,23 @@ public class MovablePlatform : MonoBehaviour , IMovable
         Vector2 smoothPos = Vector2.SmoothDamp(transform.position,targetPoint,ref _velocity,smoothTime,speed);
         
         transform.position = smoothPos;
+        foreach (CharacterVerticalCollider character in characters)
+        {
+            var rb = character.GetComponent<Rigidbody2D>();
+            if (rb != null)
+            {
+                rb.MovePosition(transform.position);
+                ColliderBlip(character.gameObject);
+            }
+            character.transform.SetParent(transform);
+        }
 
         if (Vector2.Distance(transform.position, targetPoint) <= 0.1f)
         {
             elevatorMoving = false;
             transform.position = targetPoint;
+          
+
             if (onGround)
                 GetComponent<SpriteRenderer>().sortingOrder = groundSpriteLayer;
             else
@@ -73,19 +87,36 @@ public class MovablePlatform : MonoBehaviour , IMovable
                 
             foreach (CharacterVerticalCollider character in characters)
             {
+                character.transform.SetParent(null); //desemparento siempre al llegar
+                ColliderBlip(character.gameObject);
                 if (onGround)
                 {
                     character.SetToGroundColliders();
-                    character.transform.position = transform.position;
+                    var rb = character.GetComponent<Rigidbody2D>();
+                    if (rb != null)
+                    {
+                        rb.MovePosition(transform.position);
+                    }
+                    character.transform.position = RandomPosOutPlatform(transform);
+                    ColliderBlip(character.gameObject);
+
                 }
                 else
                 {
                     character.SetToElevatedColliders();
-                    character.transform.position = dismountPosition.position;
+                    var rb = character.GetComponent<Rigidbody2D>();
+                    if (rb != null)
+                    {
+                        rb.MovePosition(dismountPosition.position);
+                    }
+                    character.transform.position = RandomPosOutPlatform(dismountPosition);
+                    ColliderBlip(character.gameObject);
                 }
             }
             characters.Clear();
-            ColliderBlip();
+            RefreshCharactersInPlatform();
+         //   ColliderBlip(char);
+         //    ColliderBlip(this.gameObject);
         }
         
     }
@@ -101,15 +132,36 @@ public class MovablePlatform : MonoBehaviour , IMovable
                     characters.Add(character);
             }
         }
-        
+
+
     }
 
-    private void ColliderBlip()
+    private void ColliderBlip(GameObject gameObject)
     {
-        GetComponent<Collider2D>().enabled = false;
-        GetComponent<Collider2D>().enabled = true;
+        if (elevatorMoving)
+        gameObject.GetComponent<Collider2D>().enabled = false;
+        else
+            gameObject.GetComponent<Collider2D>().enabled = true;
     }
-
+    private void RandomPosInPlatform()
+    {
+        float separation = usableWidth / (characters.Count + 1);
+        for (int i = 0; i < characters.Count; i++)
+        {
+            float baseOffset = -usableWidth / 2 + separation * (i + 1);
+            float jitter = Random.Range(-separation * 0.2f, separation * 0.2f);
+            Vector3 targetPos = transform.position + new Vector3(baseOffset + jitter, +1, 0);
+            characters[i].transform.position = targetPos;
+        }
+    }
+    private Vector3 RandomPosOutPlatform(Transform outPosition)
+    {
+        float separation = usableWidth / (characters.Count + 1);
+        float randomOffset = Random.Range(-usableWidth * 0.5f, usableWidth * 0.5f);
+        float jitter = Random.Range(-separation * 0.2f, separation * 0.2f);
+        Vector3 finalPos = outPosition.position + new Vector3(randomOffset, +1, 0);
+        return finalPos;
+    }
     private void OnTriggerExit2D(Collider2D collision)
     {
         if (!elevatorMoving)
@@ -119,9 +171,25 @@ public class MovablePlatform : MonoBehaviour , IMovable
             {
                 if (characters.Contains(character))
                     characters.Remove(character);
+       
             }
+
         }
         
+    }
+    private void RefreshCharactersInPlatform()
+    {
+        Collider2D platformCollider = GetComponent<Collider2D>();
+        Collider2D[] colliders = Physics2D.OverlapBoxAll(platformCollider.bounds.center, platformCollider.bounds.size, 0f);
+
+        foreach (Collider2D col in colliders)
+        {
+            CharacterVerticalCollider character = col.GetComponent<CharacterVerticalCollider>();
+            if (character != null && !characters.Contains(character))
+            {
+                characters.Add(character);
+            }
+        }
     }
 
     public void TriggerElevator()
